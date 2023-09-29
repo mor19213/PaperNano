80e3e92b4b7309881f3ea17a7c13ea697c7630df
daniela <mor19213@uvg.edu.gt>
2023-09-29 17:54:45 -0600

    append dict spacing rules

--- dict.py
+++ dict.py
@@ -8,22 +8,28 @@ def json_a_dict(path):
 
 def a_dict(dict_n, dict_e, key):
     for layer, i in dict_n.items():
-        #print(f"layer: {layer}")
-        #print(f"key: {key}")
-        #print(f"i: {i}")
+        print(f"layer: {layer}")
+        print(f"key: {key}")
+        print(f"i: {i}")
+        #if key_exists(dict_e[layer], ):
+            #print(f"{layer} existe")
         dict_e[layer][key]=i
     return dict_e
 
 arch = {}
 #arch['layerMapping'] = json_a_dict(path+'layerMapping.json')
-arch['maskNumbers'] = json_a_dict(path+'maskNumbers.json')
 arch['layerMaterials'] = json_a_dict(path+'layerMaterials.json')
+arch['maskNumbers'] = json_a_dict(path+'maskNumbers.json')
+print("spacingRules ------------------------------------------")
+spacingRules = json_a_dict(path+'spacingRules.json')
 
 reglas = json_a_dict(path+'layerMapping.json')
 
 for k, val in arch.items():
     #print(f"key: {k}")
     reglas = a_dict(val, reglas, str(k))
+
+reglas = [reglas, spacingRules]
 
 with open('reglas.json', 'w') as json_file:
     json.dump(reglas, json_file, indent=4)
